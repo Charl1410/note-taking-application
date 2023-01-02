449620f9a8dee96fbbfbdcea40e9231b567a93b0
Charl1410 <112945634+Charl1410@users.noreply.github.com>
2023-01-02 15:43:05 +0000

    added get routes for pages

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const path = require('path');
 
+//setting up port 
 const PORT = process.env.PORT || 3001;
 
+//setting up an instance of express
 const app = express()
-
+//middleware to say we are dealing with json data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
+//serving up static data from the public folder
 app.use(express.static('public'));
 
+//GET route to serve home page
 app.get('/', (req, res) =>
 res.send(path.join(__dirname, '/public/index.html'))
 );
+
+//GET route to serve notes page 
+app.get('/notes', (req, res) =>
+res.send(path.join(__dirname, '/public/notes.html'))
+);
+ 
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes at http://localhost:${PORT}`)
+);
+
 
